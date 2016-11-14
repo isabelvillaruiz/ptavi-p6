9191a5dab980a6446c03321c5c4cb77a11e2959a
isabelvillaruiz <i.villa@alumnos.urjc.es>
2016-11-14 10:57:06 +0100

    Reconoce parametros de la terminal

--- client.py
+++ client.py
@@ -5,20 +5,46 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys 
 
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+
+
+INFO = sys.argv[2]
+INFO1 = INFO.split("@")
+INFO2 = INFO1[1].split(":")
+PORT = int(INFO2[1])
+SERVER = (INFO2[0])
+
+
+print("Este es el servidor" , SERVER)
+print("Este es el puerto" , PORT)
+
+REQUEST = sys.argv[1]
+
+
+if len(sys.argv) != 3:
+    sys.exit("Usage: python client.py method receiver@IP:SIPport")
 
 # Contenido que vamos a enviar
+'''
+if REQUEST == 'Invite':
+    
+elif REQUEST == 'Bye':
+
+'''
+
+ 
+
+
 LINE = '¡Hola mundo!'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((SERVER,PORT))
 
 print("Enviando: " + LINE)
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
