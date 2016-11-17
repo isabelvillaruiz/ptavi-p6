b32a5d2b76a602cbf445d05d33b1329cbdc37db7
isabelvillaruiz <i.villa@alumnos.urjc.es>
2016-11-17 14:16:56 +0100

    FALTA CAPTURA

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
+import os
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -13,22 +14,23 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
     SERVER = (sys.argv[1])
     PORT = int(sys.argv[2])
-    song = (sys.argv[3])
-
-    MP3 = sys.argv[3][-3:]
+    SONG = (sys.argv[3])
+   
+    MP3 = sys.argv[3][-4:]
 
     #Cuando añadamos la cancion habra que poner != 4
     if len(sys.argv) != 4:
-        if sys.argv[3][-3:] != "mp3":
+        if sys.argv[3][-4:] != ".mp3":
             sys.exit("Usage: python server.py IP port audio_file")
     
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion\r\n\r\n")
-        text = self.rfile.read()
+        #self.wfile.write(b"Hemos recibido tu peticion\r\n\r\n")
+
         
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
+            text = self.rfile.read()
             line = self.rfile.read()
             print("El cliente nos manda " + text.decode('utf-8'))
             LINE = text.decode('utf-8')
@@ -37,13 +39,25 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             print("La peticion es: ", REQUEST )
             print("Listening...")
 
-            if REQUEST == 'Invite':
+            if REQUEST == 'INVITE':
                 answer100 = b"SIP/2.0 100 Trying\r\n\r\n"
                 answer180 = b"SIP/2.0 180 Ring\r\n\r\n"
-                ANSWER = answer100 + answer180
+                answer200 = b"SIP/2.0 200 OK\r\n\r\n"
+                ANSWER = answer100 + answer180 + answer200
                 self.wfile.write(ANSWER)
-                #my_socket.send(bytes(ANSWER, 'utf-8') + b'\r\n')
-            
+            elif REQUEST == 'ACK' :
+
+                
+                SONG = (sys.argv[3])
+                aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 < ' + self.SONG
+                print ('Vamos a ejecutar', aEjecutar)
+                os.system(aEjecutar)
+                
+                
+            elif REQUEST == 'BYE' :
+                self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')                
+
+  
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
@@ -51,6 +65,6 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     #Los ' ' son el localhost del servidor que en esta practica hay que especificarlo
-    serv = socketserver.UDPServer(('', int(sys.argv[2])), EchoHandler)
+    serv = socketserver.UDPServer((sys.argv[1], int(sys.argv[2])), EchoHandler)
     print("Lanzando servidor UDP de eco...")
     serv.serve_forever()
