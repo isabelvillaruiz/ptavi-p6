2b282ff7e09efb593b056ace553d0aba74488fa6
isabelvillaruiz <i.villa@alumnos.urjc.es>
2016-11-17 14:49:45 +0100

    Falta Captura  CON PEP8

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ import socketserver
 import sys
 import os
 
+
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
@@ -15,19 +16,17 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     SERVER = (sys.argv[1])
     PORT = int(sys.argv[2])
     SONG = (sys.argv[3])
-   
     MP3 = sys.argv[3][-4:]
 
     #Cuando añadamos la cancion habra que poner != 4
     if len(sys.argv) != 4:
         if sys.argv[3][-4:] != ".mp3":
             sys.exit("Usage: python server.py IP port audio_file")
-    
+
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         #self.wfile.write(b"Hemos recibido tu peticion\r\n\r\n")
 
-        
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             text = self.rfile.read()
@@ -36,7 +35,7 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             LINE = text.decode('utf-8')
             Words_LINES = LINE.split()
             REQUEST = Words_LINES[0]
-            print("La peticion es: ", REQUEST )
+            print("La peticion es: ", REQUEST)
             print("Listening...")
 
             if REQUEST == 'INVITE':
@@ -44,27 +43,24 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                 answer180 = b"SIP/2.0 180 Ring\r\n\r\n"
                 answer200 = b"SIP/2.0 200 OK\r\n\r\n"
                 ANSWER = answer100 + answer180 + answer200
-                self.wfile.write(ANSWER)
-            elif REQUEST == 'ACK' :
+                #self.wfile.write(answer100)
+                #self.wfile.write(answer180)
+                #self.wfile.write(answer200)
 
-                
+            elif REQUEST == 'ACK':
                 SONG = (sys.argv[3])
                 aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 < ' + self.SONG
                 print ('Vamos a ejecutar', aEjecutar)
                 os.system(aEjecutar)
-                
-                
-            elif REQUEST == 'BYE' :
-                self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')                
+            elif REQUEST == 'BYE':
+                self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
 
-  
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    #Los ' ' son el localhost del servidor que en esta practica hay que especificarlo
     serv = socketserver.UDPServer((sys.argv[1], int(sys.argv[2])), EchoHandler)
     print("Lanzando servidor UDP de eco...")
     serv.serve_forever()
